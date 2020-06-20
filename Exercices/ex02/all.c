3c577a83708f80ff44ca076af87c58a99f39c09b
rsoultan <romuald1.soultan@epitech.eu>
2020-06-20 16:40:33 +0200

    [FEAT]ex02, solution ex02

---
+++ Exercices/ex02/all.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** workshop_makefile
+** File description:
+** all
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("salucromu\n");
+    return (0);
+}
---
+++ Exercices/ex02/re.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** workshop_makefile
+** File description:
+** re
+*/
+
+#include <stdio.h>
+
+int titi(void)
+{
+    printf("titi\n");
+    return (0);
+}
